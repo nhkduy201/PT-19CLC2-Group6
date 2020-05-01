aaf0e3d58730a9a687ccd5aa410938efaf9ca3e5
nhkduy201 <nhkduy201@gmail.com>
2020-05-01 20:49:08 +0700

    Leader separate files for All roles part

---
+++ 19127354_W05/Project1/allroles.h
@@ -0,0 +1,28 @@
+#pragma once
+#ifndef _ALLROLE_H_
+#define _ALLROLE_H_
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<Windows.h>
+using namespace std;
+struct DateOfBirth {
+	string day, month, year;
+};
+struct User {
+	string role;
+	string name;
+	string username;
+	string password;
+	string degree = "";
+	DateOfBirth DoB = { "", "", "" };
+	int pos;
+};
+void MainMenu();
+void Login();
+bool CheckLogin(string rolepath, User* user);
+void UserMenu(User& user);
+void ClearPrintDelay(string message);
+void ClearPrintDelay();
+void ViewProfile(User user);
+#endif
